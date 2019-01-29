806fe8aa133e9f837e8953f44fdd88fcdf2cf8d2
Raúl Silva <raul.silva@contpaqi.com>
2019-01-28 19:16:51 -0600

    Se agregan ejemplos para multiples tabs

--- src/com/sitio/CreditoTest.java
+++ src/com/sitio/CreditoTest.java
@@ -1,13 +1,17 @@
 package src.com.sitio;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+
+import java.security.Key;
 import java.util.List;
 
 public class CreditoTest {
@@ -20,6 +24,8 @@ public class CreditoTest {
         //Estando en la página de selección de reportes
         ChromeDriver driver = new ChromeDriver();
         driver.get("http://localhost/thai/eCredisystem.htm");
+
+        Assertions.assertEquals("eCredisystem", driver.getTitle());
 
         //Esperamos a que el iframe de los menús de reportes esté visible para obtener referencia al iframe
         WebDriverWait waitMenu = new WebDriverWait(driver, 240);
@@ -44,5 +50,48 @@ public class CreditoTest {
         Select select = new Select(reportesMenu.findElement(By.id("s5")));
         select.selectByVisibleText("Download Reports");
         //Se debe generar el reporta aquí :)
+    }
+
+    @Test
+    public void multiplesTabs(){
+        System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
+
+        //Estando en la página de selección de reportes
+        ChromeDriver driverMain = new ChromeDriver();
+        driverMain.get("http://localhost/eCredisystem/cm6_a_Documentos.htm");
+
+        WebElement fieldSet = driverMain.findElement(By.tagName("fieldset"));
+        WebElement row = fieldSet.findElement(By.className("tbody"));
+        WebElement td = row.findElements(By.tagName("td")).get(0);
+        String link = td.findElement(By.tagName("a")).getAttribute("href");
+        Assertions.assertNotEquals("", link);
+
+        ChromeDriver tabDriver1 = new ChromeDriver();
+        tabDriver1.get(link);
+
+        ChromeDriver tabDriver2 = new ChromeDriver();
+        tabDriver2.get(link);
+
+        ChromeDriver tabDriver3 = new ChromeDriver();
+        tabDriver3.get(link);
+    }
+
+    @Test
+    public void multiplesTabsWithCtrl(){
+        System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
+
+        //Estando en la página de selección de reportes
+        ChromeDriver driverMain = new ChromeDriver();
+        driverMain.get("http://localhost/eCredisystem/cm6_a_Documentos.htm");
+
+        WebElement fieldSet = driverMain.findElement(By.tagName("fieldset"));
+        WebElement row = fieldSet.findElement(By.className("tbody"));
+        WebElement td = row.findElements(By.tagName("td")).get(0);
+        WebElement linkElement = td.findElement(By.tagName("a"));
+
+        int numeroTabs = 10;
+        for (int i = 0; i < numeroTabs; i++){
+            linkElement.sendKeys(Keys.CONTROL + "" + Keys.ENTER);
+        }
     }
 }
